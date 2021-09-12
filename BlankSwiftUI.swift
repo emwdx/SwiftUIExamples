90ea735b7d32b8b832127b45484af6a235038046
Evan Weinberg <weinbergmath@gmail.com>
2021-09-12 17:44:00 +0700

    Create BlankSwiftUI.swift

---
+++ BlankSwiftUI.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+import PlaygroundSupport
+
+struct MyApp: View{
+    
+    var body: some View{
+        VStack{
+            
+            Text("This is where you should place your SwiftUI Elements.")
+            
+            
+        }
+        
+    }
+    
+    
+}
+
+PlaygroundPage.current.setLiveView(MyApp())
+
